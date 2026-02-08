9f719a97173f03f4c753dda4f7f708e756fca930
Piotr Kurzawa <piotr.kurzawa@outlook.com>
2026-02-08 11:54:05 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,33 +1,25 @@
-# itunes-navidrome-migration
-Python scripts to transfer iTunes history to a new Navidrome installation
+# iTunes-navidrome-migration
+Python scripts to transfer iTunes history & playlists to a new Navidrome installation
+
 ## Introduction
-These Python scripts will transfer song ratings, play counts, play dates and playlists from an existing iTunes library to a new Navidrome installation.
+This Python script will transfer song ratings, play counts, play dates, added dates and playlists from an existing iTunes library to a new Navidrome installation. This is a modified script created by Stampede (https://github.com/Stampede/itunes-navidrome-migration), with the playlists being migrated using SQLite scripts instead of Navidrome API.
 
 ## Background
-Itunes saves its data in a Library.xml file. Navidrome saves its data in up to three different `navidrome.db*` files. The script reads from `Library.xml` and writes the data to the navidrome.db file.
+iTunes saves its data in a Library.xml file. Navidrome saves its data in up to three different `navidrome.db*` files. The script reads from `Library.xml` and writes the data to the navidrome.db file.
 
-These scripts were tested on Linux using a Library.xml file that came from iTunes on Windows. I'm running the Docker version of Navidrome v0.48.0 & Python version 3.10.6.
-
-I am not sure how it would work on other platforms.
-
-## Known issues
-1. If the iTunes database has a star rating for an entry but no play count or play date, then it will not migrate that rating to Navidrome. I'm not sure if this is a common enough situation to bother fixing in the ratings migration script. Let me know if it affects you.
-
-2. Foreign alphabets and characters (e.g. Japanese) may not transfer. See [this issue](https://github.com/Stampede/itunes-navidrome-migration/issues/4) for details.
+This script was tested on Navidrome v0.59.
 
 ## Installation
 1. You can create a Python virtual environment, or not.
-2. Download `itunesPlaylistMigrator.py`, `itunestoND.py` and `requirements.txt` and save to a folder.
+2. Download `iTunestoND.py` and `requirements.txt` and save to a folder.
 3. `$ pip3 install -r requirements.txt`
 
-### Not comfortable with Python?
-Put your database files in a drop box or something and I will migrate your iTunes library for $15. Email me at ylh9hhiaq@mozmail.com. For playlists, I'd send you back a web page with your playlists listed and hotlinked. If you want to keep the playlist, just click on its link and it will get added to Navidrome.
-
 ## How to use
+
 ### Preparing your library
 Set up your Navidrome server and copy all the folders and music files from your iTunes library to the Navidrome library. Navidrome will build its own database from scratch based on the file metadata. 
 
-The most important thing is that you keep the same directory structure between iTunes and Navidrome libraries. Do not rename, delete or move any files or directories. The script uses the file paths to sync the databases. If you want to reorganize the file structure, do it after you have moved over all your itunes data.
+The most important thing is that you keep the same directory structure between iTunes and Navidrome libraries. Do not rename, delete or move any files or directories. The script uses the file paths to sync the databases. If you want to reorganize the file structure, do it after you have moved over all your iTunes data.
 
 That said, before running the scripts, I found it very helpful to use Music Brainz Picard to clean up file metadata **without moving any files**. Use Navidrome for a week or so and if you have problems finding albums or songs, use Picard or Beets or something to improve the metadata tags for the files that are acting funny.
 
@@ -36,28 +28,9 @@ That said, before running the scripts, I found it very helpful to use Music Brai
 ### Migrating play counts, last played date and song ratings
 1. Shut down your Navidrome server.
 2. Copy the Navidrome database files to the machine with these scripts. In my case there are 3 database files: `navidrome.db`, `navidrome.db-shm` and `navidrome.db-wal` you need any `navidrome.db*` file that you find.
-3. Run the first script: `$python3 itunestoND.py`
-4. It will prompt you to type the path to the `navidrome.db` and `Library.xml`
-5. Wait. For large libraries, it can take a few minutes to crunch all the data in `Library.xml`.
-6. When it's done, your Navidrome database files may be collapsed into a single `navidrome.db` file. This is OK.
-7. **On the machine with the ND server:** delete the 3 database files, then copy over the `navidrome.db` file from the script. Put it in their place.
-8. Start your Navidrome server to make sure everything worked correctly. You should now have song ratings and play counts.
+3. Run the first script: `$python3 iTunestoND.py <navidrome_db_path> <iTunes_db_path>`
+4. Wait. For large libraries, it can take a few minutes to crunch all the data in `Library.xml`.
+5. When it's done, your Navidrome database files may be collapsed into a single `navidrome.db` file. This is OK.
+6. **On the machine with the ND server:** delete the 3 database files, then copy over the `navidrome.db` file from the script. Put it in their place.
+7. Start your Navidrome server to make sure everything worked correctly. You should now have song rating, play counts and playlists.
 
-The script will also generate a file called `IT_file_correlations.py`. If you don't want to move over your iTunes playlists, you can just delete this.
-
-### Migrating iTunes playlists
-I wrote this as an afterthought, so these instructions are a little weird. This script will not move smart playlists. Because of the way `Library.xml` is structured, there are sometimes "false positives" when looking for playlists. You will be prompted before each playlist is created. If you don't recognize a list, just decline when it asks if you want to transfer it.
-
-1. Backup your Navidrome database like you did for the last script in case something goes wrong.
-2. Make sure your Navidrome server is running.
-3. The previous script generated a file: `IT_file_correlations.py`. Move that file into the same directory where you have `itunesPlaylistMigrator.py` stored.
-4. Run the playlist migrator script: `$ python3 itunesPlaylistMigrator.py`. If your working directory is not the same where `Library.xml` is stored, you will be prompted for the path to `Library.xml`.
-5. Answer the prompts for your Navidrome username and password.
-6. The script will search for playlists from your iTunes library and prompt if you want to move them to Navidrome.
-
-## Acknowledgments
-Thanks to the Navidrome developers for their hard work and for putting up with my basic questions on the Discord chat as I worked on this script.
-
-Hopefully these scripts help some people. Feel free to copy / share / improve etc.. As far as I'm concerned, this is public domain.
-
-Hello.
---
+++ itunes-nd.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+
+import sys, sqlite3, datetime, re, string, random
+from pathlib import Path
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+import unicodedata
+import demoji
+
+def determine_userID(nd_p):
+    conn = sqlite3.connect(nd_p)
+    cur = conn.cursor()
+    cur.execute('SELECT id, user_name FROM user')
+    users = cur.fetchall()
+    if len(users) == 1:
+        print(f'Changes will be applied to the {users[0][1]} Navidrome account.')
+    else:
+        raise Exception('There needs to be exactly one user account set up with Navidrome. You either have 0, or more than 1 user account.')
+    conn.close()
+    return users[0][0]
+
+def update_playstats(d1, id, playcount, playdate, rating=0, starred=0):
+    d1.setdefault(id, {})
+    d1[id].setdefault('play count', 0)
+    d1[id].setdefault('play date', datetime.datetime.fromordinal(1))
+    d1[id]['play count'] += playcount
+    d1[id]['rating'] = rating
+    d1[id]['starred'] = starred
+
+    if playdate > d1[id]['play date']: d1[id].update({'play date': playdate})
+
+def write_to_annotation(userID, dictionary_with_stats, item_type):
+    annotation_entries = []
+    for item_id in dictionary_with_stats:
+        this_entry = dictionary_with_stats[item_id]
+        
+        play_count = this_entry['play count']
+        play_date = this_entry['play date'].strftime('%Y-%m-%d %H:%M:%S') # YYYY-MM-DD 24:mm:ss
+        rating = this_entry['rating']
+        starred = this_entry['starred']
+
+        annotation_entries.append((userID, item_id, item_type, play_count, play_date, rating, starred, None, None))
+
+    conn = sqlite3.connect(nddb_path)
+    cur = conn.cursor()
+    cur.executemany('INSERT INTO annotation VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', annotation_entries)
+    conn.commit()
+    conn.close()
+
+def update_dates(d1, id, date_added, date_modified):
+    d1.setdefault(id, {})
+    d1[id].setdefault('created at', datetime.datetime.fromordinal(1))
+    d1[id].setdefault('updated at', datetime.datetime.fromordinal(1))
+
+    if date_added > d1[id]['created at']: 
+        d1[id].update({'created at': date_added})
+    if date_modified > d1[id]['updated at']: 
+        d1[id].update({'updated at': date_modified})
+
+def write_dates(dictionary_with_dates, entry_type):
+    conn = sqlite3.connect(nddb_path)
+    cur = conn.cursor()
+
+    for item_id in dictionary_with_dates:
+        this_entry = dictionary_with_dates[item_id]
+        
+        created_at = this_entry['created at'].strftime('%Y-%m-%d %H:%M:%S') # YYYY-MM-DD 24:mm:ss
+        updated_at = this_entry['updated at'].strftime('%Y-%m-%d %H:%M:%S') # YYYY-MM-DD 24:mm:ss
+        if entry_type == 'album':
+            cur.execute('''UPDATE album SET created_at = ?, updated_at = ? WHERE id = ?''', (created_at, updated_at, item_id))
+        if entry_type == 'media_file':
+            cur.execute('''UPDATE media_file SET created_at = ?, updated_at = ? WHERE id = ?''', (created_at, updated_at, item_id))
+    conn.commit()
+    conn.close()
+
+def insert_playlist(playlist_name, user_id):
+    conn = sqlite3.connect(nddb_path)
+    cur = conn.cursor()
+    cur.execute('''SELECT id FROM playlist WHERE name = ?''', (playlist_name,))
+    playlist_id = cur.fetchone()
+
+    if playlist_id is None:
+        # navidrome playlist id is 22 character long and doesn't contain '-'
+        playlist_id = ''.join(random.choices(string.ascii_letters + string.digits, k=22))
+        cur.execute('''INSERT INTO playlist (id,name,owner_id,created_at,updated_at) VALUES (?, ?, ?, ?, ?)''', (playlist_id, playlist_name, user_id, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        conn.commit()
+
+    conn.close()
+    return playlist_id[0]
+
+def insert_playlist_track(id, playlist_id, track_id):
+    conn = sqlite3.connect(nddb_path)
+    cur = conn.cursor()
+    cur.execute('''INSERT OR IGNORE INTO playlist_tracks (id, playlist_id, media_file_id) VALUES (?, ?, ?)''', (id, playlist_id, track_id))
+    conn.commit()
+    conn.close()
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python itunes-nd.py <navidrome_db_path> <iTunes_db_path>")
+        sys.exit(1)
+    
+    global nddb_path, itdb_path
+    nddb_path = Path(sys.argv[1])
+    itdb_path = Path(sys.argv[2])
+    
+    if not nddb_path.is_file():
+        print(f"Error: Navidrome database file '{nddb_path}' not found.")
+        sys.exit(1)
+    
+    if not itdb_path.is_file():
+        print(f"Error: iTunes database file '{itdb_path}' not found.")
+        sys.exit(1)
+
+    with open(itdb_path, 'r', encoding="utf-8") as f: 
+        soup = BeautifulSoup(f, 'lxml-xml')
+
+    it_root_music_path = unquote(soup.find('key', text='Music Folder').next_sibling.text, encoding='utf-8')
+    # example output of previous line: 'file://localhost/C:/Users/REDACTED/Music/iTunes/iTunes Music/'
+
+    songs = soup.dict.dict.find_all('dict') # yields result set of media files to loop through
+    playlists = soup.array.find_all('dict', recursive=False)
+
+    song_count = len(songs)
+    print(f'Found {song_count:,} files in iTunes database to process.')
+    del(soup)
+
+    userID = determine_userID(nddb_path)
+    songID_correlation = {} # we'll save this for later use to transfer iTunes playlists to ND (another script)
+    artists = {}            # artists and albums will keep count of plays and play dates for each
+    albums = {}
+    files = {}
+
+    status_interval = song_count // 8
+    counter = 0
+
+    conn = sqlite3.connect(nddb_path)
+    cur = conn.cursor()
+    cur.execute('DELETE FROM annotation')
+    conn.commit()
+
+    for it_song_entry in songs:
+        counter += 1    # progress tracking feedback
+        if counter % status_interval == 0:
+            print(f'{counter:,} files parsed so far of {song_count:,} total songs.')
+
+        # chop off first part of IT path so we can correlate it to the entry in the ND database
+        
+        if it_song_entry.find('key', string='Location') == None: 
+            continue
+
+        song_path = unquote(it_song_entry.find('key', string='Location').next_sibling.text, encoding='utf-8')
+        if not song_path.startswith(it_root_music_path):  # excludes non-local content
+            continue   
+
+        song_path = re.sub(it_root_music_path, '', song_path)
+
+        try:
+            cur.execute('SELECT id, artist_id, album_id FROM media_file WHERE path LIKE "%' + song_path + '"')
+            song_id, artist_id, album_id = cur.fetchone()
+        except TypeError:
+            # the file path might use different unicode normalization than the database
+            # there seems to not be a single one that works for all files, so we'll try
+            # all of them until we find one that works
+            found = False
+            for x in ['NFC', 'NFD', 'NFKC', 'NFKD']:
+                try:
+                    song_path = unicodedata.normalize(x, song_path)
+                except UnicodeDecodeError:
+                    continue
+                try:
+                    cur.execute('SELECT id, artist_id, album_id FROM media_file WHERE path LIKE "%' + song_path + '"')
+                    song_id, artist_id, album_id = cur.fetchone()
+                    found = True
+                    break
+                except TypeError:
+                    continue
+
+            if not found:
+                # try removing emojis from the path
+                song_path_no_emoji = demoji.replace(song_path, '????') # according to some sources, emojis are replaced with 4 question marks in Navidrome DB
+                try:
+                    cur.execute('SELECT id, artist_id, album_id FROM media_file WHERE path LIKE "%' + song_path_no_emoji + '"')
+                    song_id, artist_id, album_id = cur.fetchone()
+                    found = True
+                except TypeError:
+                    continue
+
+            if not found:
+                print(f"Error while parsing {song_path}. Cannot find the matching entry in Navidrome database. SQL query: ")
+                print('SELECT id, artist_id, album_id FROM media_file WHERE path LIKE "%' + song_path + '"')
+                continue
+
+        # correlate iTunes ID with Navidrome ID (for use in a future script)
+        it_song_ID = int(it_song_entry.find('key', string='Track ID').next_sibling.text)
+        songID_correlation.update({it_song_ID: song_id})
+        
+        try:    # get rating, play count & date from iTunes
+            song_rating = int(it_song_entry.find('key', string='Rating').next_sibling.text)
+            song_rating = int(song_rating / 20)
+        except AttributeError: 
+            song_rating = 0 # rating = 0 (unrated) if it's not rated in iTunes
+            
+        try:
+            play_count = int(it_song_entry.find('key', string='Play Count').next_sibling.text)
+            last_played = it_song_entry.find('key', string='Play Date UTC').next_sibling.text[:-1] # slice off the trailing 'Z'
+            last_played = datetime.datetime.strptime(last_played, '%Y-%m-%dT%H:%M:%S') # convert from string to datetime object. Example string: '2020-01-19T02:24:14Z'
+            date_added = it_song_entry.find('key', string='Date Added').next_sibling.text[:-1]
+            date_added = datetime.datetime.strptime(date_added, '%Y-%m-%dT%H:%M:%S')
+            date_modified = it_song_entry.find('key', string='Date Modified').next_sibling.text[:-1]
+            date_modified = datetime.datetime.strptime(date_modified, '%Y-%m-%dT%H:%M:%S')
+        except AttributeError: 
+            continue
+
+        update_playstats(artists, artist_id, play_count, last_played)
+        update_playstats(albums, album_id, play_count, last_played)
+        update_playstats(files, song_id, play_count, last_played, rating=song_rating)
+        update_dates(albums, album_id, date_added, date_modified)
+        update_dates(files, song_id, date_added, date_modified)  
+
+    playlists_to_skip = ('Library', 'Downloaded', 'Music', 'Movies', 'TV Shows', 'Podcasts', 'Audiobooks', 'Tagged', 'Genius')
+    for plist in playlists:
+        if plist.find('key', text='Distinguished Kind'): 
+            continue # these are special playlists unique to iTunes
+        if plist.find('key', text='Visible'):
+            if plist.find('key', text='Visible').find_next('True') == None: 
+                continue # skip invisible playlists - like Library
+        
+        playlist_name = plist.find('key', text='Name').find_next('string').text
+        if playlist_name in playlists_to_skip: 
+            continue
+        if plist.find('key', text='Smart Info'): 
+            continue
+        
+        try:
+            playlist_tracks = plist.array.find_all('dict')
+        except AttributeError:
+            continue
+
+        playlist_id = insert_playlist(playlist_name, userID)
+
+        playlist_track_no = 0;
+        for track in playlist_tracks:
+            playlist_track_no += 1
+            try:
+                nd_track_id = songID_correlation[int(track.integer.text)]
+                insert_playlist_track(playlist_track_no, playlist_id, nd_track_id)
+            except KeyError:
+                print(f"Error while parsing playlist {playlist_name}. Cannot find the matching entry in Navidrome database for iTunes track ID {track.integer.text}. Playlist track number in iTunes playlist: {playlist_track_no}. ")
+                continue
+    
+    conn.close()
+
+    write_to_annotation(userID, artists, 'artist')
+    write_to_annotation(userID, files, 'media_file')
+    write_to_annotation(userID, albums, 'album')
+    write_dates(files, 'media_file')
+    write_dates(albums, 'album')
+
+if __name__ == "__main__":
+    main()
